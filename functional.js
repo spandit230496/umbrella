b030b454c2f72428f6f1a529a5c12b0cb70ebdc2
spandit230496 <sandip.pandit230496@gmail.com>
2023-04-25 10:27:47 +0530

    coding assigment is in folder with name coding assigment

---
+++ functional.js
@@ -0,0 +1,37 @@
+
+function myFunction() {
+ 
+    document.getElementById("myImg").src = "Yellow.png";
+    document.getElementById("upload").style.backgroundColor="yellow"
+    document.getElementById("container").style.backgroundColor="#ffe9a3"
+  }
+  function myFunction2() {
+   document.getElementById("myImg").src = "Pink.png";
+   document.getElementById("upload").style.backgroundColor="pink"
+   document.getElementById("container").style.backgroundColor="#F3D1DD"
+  }
+  function myFunction3() {
+    document.getElementById("myImg").src = "Blue.png";
+    document.getElementById("upload").style.backgroundColor="blue"
+    document.getElementById("container").style.backgroundColor=" #7D95FF"
+  }
+
+function onUpload(){
+    
+    var x = document.getElementById("myText").files
+    document.getElementById("txt").innerHTML =x[0].name;
+   
+}
+const image_input =document.getElementById("myText")
+var uploaded_image="";
+
+ image_input.addEventListener("change", function(){
+  const reader= new FileReader();
+
+  reader.addEventListener("load", ()=>{
+    //  document.getElementById("loader").style.display=none;
+    uploaded_image=reader.result;
+    document.getElementById("logo").src=uploaded_image
+  });reader.readAsDataURL(this.files[0]);
+})
+
